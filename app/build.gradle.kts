ca74372689696f8bbf8de5b0c4c64b6b69805693
yuan <yuan0x00@gmail.com>
2025-11-30 16:07:52 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,7 @@ dependencies {
     implementation(libs.koin.core)
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
+    implementation(libs.ktor.client.android )
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
 }
