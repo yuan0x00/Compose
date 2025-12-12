13e09a716a00b29ba2dead7b0da8db784018fa4c
yuan <yuan0x00@gmail.com>
2025-12-12 23:51:20 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,20 +9,39 @@ val properties = Properties()
 properties.load(FileInputStream(rootProject.file("config.properties")))
 
 android {
-    namespace = properties.getProperty("namespace")
+    namespace = "com.rapid.compose"
 
     defaultConfig {
-        applicationId = namespace
+        applicationId = properties.getProperty("applicationId")
         versionCode = properties.getProperty("versionCode").toInt()
         versionName = properties.getProperty("versionName")
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildFeatures {
         buildConfig = true
     }
+    signingConfigs {
+        getByName("debug") {
 
+        }
+        maybeCreate("release").apply {
+            storeFile = project.rootProject.file("./app/release.jks")
+            storePassword = properties.getProperty("storePassword")
+            keyAlias = properties.getProperty("keyAlias")
+            keyPassword = properties.getProperty("keyPassword")
+        }
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
 }
 
 dependencies {
@@ -37,7 +56,7 @@ dependencies {
     implementation(libs.koin.core)
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
-    implementation(libs.ktor.client.android )
+    implementation(libs.ktor.client.android)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
 }
